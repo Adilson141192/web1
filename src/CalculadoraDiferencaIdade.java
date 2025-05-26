dd8e89aab7846ebacfbd94c3d846717a08dc3db5
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-05-26 10:30:57 -0300

    052625 10

--- src/CalculadoraDiferencaIdade.java
+++
@@ -1,8 +0,0 @@
-public class CalculadoraDiferencaIdade {
-    public static void main(String[] args) {
-        int primeiraIdade = 30;
-        int segundaIdade = 15;
-        int diferencaIdade = primeiraIdade - segundaIdade;
-        System.out.println("A diferença entra as idades é: " + diferencaIdade);
-    }
-}
---
+++ src/CalculatorDifferencing.java
@@ -0,0 +1,8 @@
+public class CalculatorDifferencing {
+    public static void main(String[] args) {
+        int firstAge = 30;
+        int secondAge = 15;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("A diferença entra as idades é: " + ageDifference);
+    }
+}
