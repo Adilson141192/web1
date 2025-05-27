9a6c67ace4d759f14b963f5f8eb9ecc0c43dcefa
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-05-27 10:18:10 -0300

    052725 11

---
+++ src/taxCalculator.java
@@ -0,0 +1,8 @@
+public class taxCalculator {
+    public static void main(String[] args) {
+        float salario = 2500.00F;
+        float porcentagem = 30F;
+        float porcentagemSalario = salario * porcentagem / 100;
+        System.out.println(porcentagemSalario);
+    }
+}
