905ff1a7f9d49e9c91b794a8a83b15b332917796
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-05-28 09:13:22 -0300

    052825 15

---
+++ src/verificaIdade.java
@@ -0,0 +1,11 @@
+public class verificaIdade {
+    public static void main(String[] args) {
+        int idade = 18;
+
+        if (idade < 18) {
+            System.out.println("Ainda não é adulto");
+        } else {
+            System.out.println("É adulto");
+        }
+    }
+}
