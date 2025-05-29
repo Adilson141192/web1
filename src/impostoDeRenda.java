6aec64b452dbf5cf81e615e690767eb07fa65f0c
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-05-29 09:25:16 -0300

    052925 17

---
+++ src/impostoDeRenda.java
@@ -0,0 +1,16 @@
+public class impostoDeRenda {
+    public static void main(String[] args) {
+        double salario = 5350;
+        if (salario <= 1903.98) {
+            System.out.println("ISENTO");
+        } else if (salario >= 1903.99 && salario <= 2826.65) {
+            System.out.println("SEU IR É DE 7,5% = " + (salario * 0.075));
+        } else if (salario >= 2826.66 && salario <= 3751.05) {
+            System.out.println("SEU IR É DE 15% = " + (salario * 0.15));
+        } else if (salario >= 3751.06 && salario <= 4664.68) {
+            System.out.println("SEU IR É DE 22,5% = " + (salario * 0.225));
+        } else {
+            System.out.println("SEU IR É DE 27,5% = " + (salario * 0.275));
+        }
+    }
+}
