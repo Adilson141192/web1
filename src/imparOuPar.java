7cc759db468f202a513cd736ba61f6ae273050ad
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-05-29 09:04:06 -0300

    052925 16

---
+++ src/imparOuPar.java
@@ -0,0 +1,10 @@
+public class imparOuPar {
+    public static void main(String[] args) {
+        int numero = 121212112;
+        if (numero % 2 == 0) {
+            System.out.println("PAR: " + (numero % 2));
+        } else {
+            System.out.println("IMPAR: " + (numero % 2));
+        }
+    }
+}
