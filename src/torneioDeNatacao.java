b07dcd2e162a17fdd0fa84218d8a1e04f3eaa500
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-05-29 10:07:24 -0300

    052925 18

---
+++ src/torneioDeNatacao.java
@@ -0,0 +1,15 @@
+public class torneioDeNatacao {
+    public static void main(String[] args) {
+        String nome = "Luffy";
+        int idade = 19;
+        if (idade <= 10) {
+            System.out.println(nome + "Participará da categoria Infantil");
+        } else if (idade >= 11 && idade <= 15) {
+            System.out.println(nome + "Participará da categoria Juvenil");
+        } else if (idade >= 16 && idade <= 19) {
+            System.out.println(nome + "Participará da categoria Pré-adulto");
+        } else {
+            System.out.println(nome + "Participará da categoria Adulto");
+        }
+    }
+}
