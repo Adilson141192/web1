3df1662cd833766b9038a617ec4144fb13a1bc19
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-05-30 08:46:04 -0300

    053025 20

--- src/torneioDeNatacao.java
+++ src/torneioDeNatacao.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 public class torneioDeNatacao {
     public static void main(String[] args) {
-        String nome = "Luffy";
-        int idade = 19;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Escreva o seu nome: ");
+        String nome = input.next();
+        System.out.println("Escreva o seu nome: ");
+        int idade = input.nextInt();
+
         if (idade <= 10) {
-            System.out.println(nome + "Participará da categoria Infantil");
+            System.out.println(nome + " Participará da categoria Infantil");
         } else if (idade >= 11 && idade <= 15) {
-            System.out.println(nome + "Participará da categoria Juvenil");
+            System.out.println(nome + " Participará da categoria Juvenil");
         } else if (idade >= 16 && idade <= 19) {
-            System.out.println(nome + "Participará da categoria Pré-adulto");
+            System.out.println(nome + " Participará da categoria Pré-adulto");
         } else {
-            System.out.println(nome + "Participará da categoria Adulto");
+            System.out.println(nome + " Participará da categoria Adulto");
         }
     }
 }
