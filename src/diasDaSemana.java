d121fd746317fe37977f55046ca3c36132069ece
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-02 08:59:20 -0300

    060225 24

---
+++ src/diasDaSemana.java
@@ -0,0 +1,23 @@
+public class diasDaSemana {
+    public static void main(String[] args) {
+        int dia = 8;
+
+        if (dia == 1) {
+            System.out.println("Segunda");
+        } else if (dia == 2) {
+            System.out.println("Terça");
+        } else if (dia == 3) {
+            System.out.println("Quarta");
+        } else if (dia == 4) {
+            System.out.println("Quinta");
+        } else if (dia == 5) {
+            System.out.println("Sexta");
+        } else if (dia == 6) {
+            System.out.println("Sábado");
+        } else if (dia == 7) {
+            System.out.println("Domingo");
+        } else {
+            System.out.println("dia inválido");
+        }
+    }
+}
