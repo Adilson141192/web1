8846f37f077527c09c3fb15d5fa779f1aea86e7f
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-02 08:34:52 -0300

    060225 22

---
+++ src/cadastroLogin.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class cadastroLogin {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o seu nome");
+        String nome = input.next();
+        if (nome.equals("") ||
+                nome.equalsIgnoreCase("admin") ||
+                nome.equalsIgnoreCase("administrador")) {
+            System.out.println("Usuário inválido");
+        } else {
+            System.out.println(nome + " cadastrado com sucesso");
+        }
+    }
+}
