5053a96e925ad006a1634178922026f19e627703
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-04 08:56:36 -0300

    060425 28

---
+++ src/academy/devdojo/lacosDeRepeticao/aula01Introducao.java
@@ -0,0 +1,12 @@
+package academy.devdojo.lacosDeRepeticao;
+
+public class aula01Introducao {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador <= 10) {
+            System.out.println("Contador: "+ contador );
+            contador = contador + 1;
+        }
+    }
+}
