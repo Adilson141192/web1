1e3c64176cb4036aa20530a3f6b632f29ca7b386
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-05 11:10:27 -0300

    060525 33

---
+++ src/academy/devdojo/lacosDeRepeticao/fors/aula01IntroducaoFors.java
@@ -0,0 +1,9 @@
+package academy.devdojo.lacosDeRepeticao.fors;
+
+public class aula01IntroducaoFors {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
