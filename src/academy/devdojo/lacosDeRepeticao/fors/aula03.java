eed180cc763e61fe618cb748827e53bbcc7211fe
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-09 08:37:31 -0300

    060925 35 2

--- src/academy/devdojo/lacosDeRepeticao/fors/aula03.java
+++ src/academy/devdojo/lacosDeRepeticao/fors/aula03.java
@@ -5,7 +5,7 @@ public class aula03 {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Fazendo tabuada do número: " + i);
             for (int j = 1; j <= 10; j++) {
-                System.out.println(i + "x" + j);
+                System.out.println(i + "x" + j + " = " + (i*j));
             }
         }
     }
