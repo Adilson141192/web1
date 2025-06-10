f2024c21d0191860a71c1cc7ed4d4d7400e1b6b9
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-10 10:55:57 -0300

    061025 39

---
+++ src/academy/devdojo/arrays/aula03Exercicios2.java
@@ -0,0 +1,22 @@
+package academy.devdojo.arrays;
+
+import java.util.Scanner;
+
+public class aula03Exercicios2 {
+    public static void main(String[] args) {
+        int [] array1 = new int [3];
+        int [] array2 = new int [3];
+        int [] array3 = new int [3];
+        Scanner scanner = new Scanner(System.in);
+        String resultadoMulti = "";
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println("Vetor 1, posição: " + i + ": ");
+            array1[i] = scanner.nextInt();
+            System.out.println("Vetor 2, posição: " + i + ": ");
+            array2[i] = scanner.nextInt();
+            array3[i] = array1[i] * array2[i];
+            resultadoMulti = resultadoMulti + array3[i] + " 2";
+        }
+        System.out.println(resultadoMulti);
+    }
+}
