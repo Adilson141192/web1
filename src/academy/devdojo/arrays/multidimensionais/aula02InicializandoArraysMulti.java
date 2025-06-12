da10fb2440ca2c2c9f7f11001a5b2f53efb8bb4c
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-12 14:33:01 -0300

    061225 41

---
+++ src/academy/devdojo/arrays/multidimensionais/aula02InicializandoArraysMulti.java
@@ -0,0 +1,27 @@
+package academy.devdojo.arrays.multidimensionais;
+
+import java.util.Scanner;
+
+public class aula02InicializandoArraysMulti {
+    public static void main(String[] args) {
+        int [][] arrayMulti = new int[2][3];
+//        arrayMulti[0][0] = 22;
+//        arrayMulti[0][1] = 11;
+//        arrayMulti[0][2] = 54;
+//        arrayMulti[1][0] = 44;
+//        arrayMulti[1][1] = 56;
+//        arrayMulti[1][2] = 76;
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < arrayMulti.length; i++) {
+            for (int j = 0; j < arrayMulti[i].length; j++) {
+                System.out.println("Digite o valor da posição: [" + i + "][" + j + "]");
+                arrayMulti[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < arrayMulti.length; i++) {
+            for (int j = 0; j < arrayMulti[i].length; j++) {
+                System.out.println("[" + i + "][" + j + "]= " + arrayMulti[i][j]);
+            }
+        }
+    }
+}
